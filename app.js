ac5fe74d9fa9de445c14555d1064c14f01af9453
Ujjwol Kayastha <uzol123@gmail.com>
2019-05-03 12:02:45 +0545

    Authentication SIGNUP

--- app.js
+++ app.js
@@ -3,12 +3,24 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
+
+
+var session = require('express-session')
+var fileStore = require('session-file-store')(session)
+
+const User = require('./models/users')
+
+
 const url = 'mongodb://localhost:27017/mydb';
-const connect = mongoose.connect(url, { useNewUrlParser: true });
+const connect = mongoose.connect(url, {
+    useNewUrlParser: true
+});
 
 connect.then((db) => {
     console.log("Connected to mongodb server");
-}, (err) => { console.log(err); });
+}, (err) => {
+    console.log(err);
+});
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -20,64 +32,175 @@ var app = express();
 //middleware
 app.use(logger('dev'));
 app.use(express.json()); // same as bodyParser.json()
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(express.urlencoded({
+    extended: false
+}));
+// app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-function auth(req, res, next){
-    console.log(req.headers);
-    let authHeader = req.headers.authorization;
-    if (!authHeader) {
-        let err = new Error("You are not authenticated!");    
-        res.setHeader("WWW-Authenticate", "Basic");    
-        err.status = 401;
-        return next(err); 
-     }
+app.use(
+    session({
+        name: 'session-id',
+        secret: 'secret-key',
+        saveUninitialized: false,
+        resave: false,
+        store: new fileStore()
+    })
+)
 
+app.use(cookieParser("my-sercret-key"));
 
-
-    let auth = new Buffer.from(authHeader.split(" ")[1], "base64")    
-    .toString()    
-    .split(":");
-    let username = auth[0];
-    let password = auth[1];
-    //   Default username and password
-    if(username == 'admin' && password== 'admin') {      
-        next(); // authorized  
-    } else {let err = newError('You are not authenticated!');      
-    res.setHeader('WWW-Authenticate', "Basic");      
-    err.status = 401;      
-    return next(err);  }
-    // console.log(req.headers);
-    // let authHeader = req.headers.authorization;
-    // if (!authHeader) {
-    //     let err = new Error("You are not authenticated!");    
-    //     res.setHeader("WWW-Authenticate", "Basic");    
-    //     err.status = 401;
-    //     return next(err); 
-    //  }
-
-    // let auth = new Buffer.from(authHeader.split(" ")[1], "base64").toString().split()
-    // let username = auth[0]
-    // let password = auth[1]
-
-    // if (username == 'admin' && password =='admin'){
-    //     next()
-    // }else{
-    //     let err = new Error("NOT AUTHORIZED")
-    //     res.setHeader("WWW-Authenticate", "Basic")
-    //     err.status = 401
-    //     return next(err)
-    // }
+function auth(req, res, next) {
+    if (!req.session.user) {
+        let authHeader = req.headers.authorization;
+        if (!authHeader) {
+            let err = new Error("You are not authenticated!");
+            res.setHeader("WWW-Authenticate", "Basic");
+            err.status = 401;
+            return next(err);
+        }
+        let auth = new Buffer.from(authHeader.split(" ")[1], "base64").toString().split(":");
+        User.findOne({
+            username: auth[0]
+        }).then(user => {
+            if (user == null) {
+                let err = new Error("User " + auth[0] + " does not exist!");
+                res.setHeader("WWW-Authenticate", "Basic");
+                err.status = 401;
+                return next(err);
+            } else if (user.password !== auth[1]) {
+                let err = new Error("User password does not match!");
+                res.setHeader("WWW-Authenticate", "Basic");
+                err.status = 401;
+                return next(err);
+            }
+            if (user.username === auth[0] && user.password === auth[1]) {
+                req.session.user = "authenticated";
+                next();
+            } else {
+                let err = new Error("You are not authenticated!");
+                res.setHeader("WWW-Authenticate", "Basic");
+                err.status = 401;
+                next(err);
+            }
+        }).catch(err => next(err));
+    } else {
+        if (req.session.user === "authenticated") {
+            next();
+        } else {
+            let err = new Error("You are not authenticated!");
+            err.status = 401;
+            return next(err);
+        }
+    }
 }
 
+//SESSION LAGAAYERA GAREKO + COOKIES NI CHHA
+// function auth(req, res, next) {
+//     if (!req.session.user) {
+//         let authHeader = req.headers.authorization;
+//         if (!authHeader) {
+//             let err = new Error("You are not authenticated!");
+//             res.setHeader("WWW-Authenticate", "Basic");
+//             err.status = 401;
+//             return next(err);
+//         }
+//         let auth = new Buffer.from(authHeader.split(" ")[1], "base64")
+//             .toString()
+//             .split(":");
+//         if (auth[0] === "admin" && auth[1] === "secret") {
+//             // res.cookie("user", "admin", { signed: true });      
+//             req.session.user = "admin"
+//             next(); // authorized    
+//         } else {
+//             let err = new Error("Username and password do not match!");
+//             res.setHeader("WWW-Authenticate", "Basic");
+//             err.status = 401;
+//             return next(err);
+//         }
+//     } else {
+//         if (req.session.user === "admin") {
+//             console.log("req.session: ", req.session);
+//             next();
+//         } else {
+//             let err = new Error("Your cookies do not match");
+//             err.status = 401;
+//             return next(err);
+//         }
+//     }
+// }
 
-app.use(auth);
+// app.use(cookieParser("my-secret-key"));
+
+// function auth(req, res, next){
+//     console.log(req.headers);
+
+//     if(req.signedCookies.user){
+
+//         let authHeader = req.headers.authorization;
+//         if (!authHeader) {
+//             let err = new Error("You are not authenticated!");    
+//             res.setHeader("WWW-Authenticate", "Basic");    
+//             err.status = 401;
+//             return next(err); 
+//         }
+
+
+
+//     let auth = new Buffer.from(authHeader.split(" ")[1], "base64")    
+//     .toString()    
+//     .split(":");
+//     let username = auth[0];
+//     let password = auth[1];
+//     //   Default username and password
+//     if(username == 'admin' && password== 'admin') {      
+//         next(); // authorized  
+//     } else {let err = newError('You are not authenticated!');      
+//     res.setHeader('WWW-Authenticate', "Basic");      
+//     err.status = 401;      
+//     return next(err);  }
+//     // console.log(req.headers);
+//     // let authHeader = req.headers.authorization;
+//     // if (!authHeader) {
+//     //     let err = new Error("You are not authenticated!");    
+//     //     res.setHeader("WWW-Authenticate", "Basic");    
+//     //     err.status = 401;
+//     //     return next(err); 
+//     //  }
+
+//     // let auth = new Buffer.from(authHeader.split(" ")[1], "base64").toString().split()
+//     // let username = auth[0]
+//     // let password = auth[1]
+
+//     // if (username == 'admin' && password =='admin'){
+//     //     next()
+//     // }else{
+//     //     let err = new Error("NOT AUTHORIZED")
+//     //     res.setHeader("WWW-Authenticate", "Basic")
+//     //     err.status = 401
+//     //     return next(err)
+//     // }
+//     }
+
+//     // else {
+//     //     if (req.signedCookies.user === "admin") {      
+//     //         next();    
+//     //     } else {
+//     //         let err = new Error("Your cookies do not match");      
+//     //         err.status = 401;
+//     //         return next(err);    }
+//     // }
+
+// }
+
+
 
 
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use(auth);
+
 app.use('/heroes', heroesRouter);
 app.use('/villains', villainsRouter);
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,56 @@
 var express = require('express');
+
+const User = require('../models/users')
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+  // res.send('respond with a resource');
+  User.find({})
+    .then((users) => {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+      res.json(users);
+    }, (err) => next(err))
+    .catch((err) => next(err));
+});
+
+router.post("/signup", (req, res, next) => {
+  User.findOne({
+    username: req.body.username
+  }).then(user => {
+    if (user != null) {
+      let err = newError("User " + req.body.username + " already exists!");
+      err.status = 403;
+      return next(err);
+    } else {
+      return User.create({
+        username: req.body.username,
+        password: req.body.password,
+        admin: req.body.admin
+      });
+    }
+  }).then(user => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    res.json({
+      status: "Registration Successful",
+      user: user
+    });
+  }, err => next(err)).catch(err => next(err));
+});
+
+
+router.get("/logout", (req, res, next) => {
+  if (req.session.user) {
+    req.session.destroy();
+    res.clearCookie("session-id");
+    res.redirect("/");
+  } else {
+    let err = new Error("You are not logged in!");
+    err.status = 403;
+    next(err);
+  }
 });
 
 module.exports = router;
